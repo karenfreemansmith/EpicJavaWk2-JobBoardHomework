2ac7e77f31f4888be9065e5ad4d63f2c8b1e578a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-13 17:37:37 -0700

    test for job object, pass

--- src/test/java/JobTest.java
+++ src/test/java/JobTest.java
@@ -2,5 +2,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class JobTest {
+  private Job job = new Job();
+
+  @Test
+  public void job_instantiateObject_true() {
+    assertEquals(true, job instanceof Job);
+  }
 
 }
